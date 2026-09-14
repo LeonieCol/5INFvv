a7fc97cefd2587f077d2ae84190a09509bca744d
LeonieCol <leoniecolemont@gmail.com>
2026-09-14 13:33:55 +0200

    oef 4

---
+++ 2.4_oef4_basisvariabelen.py
@@ -0,0 +1,8 @@
+#geboortejaar van gebruiker
+geboortejaar_gebruiker = input("Geef het jaar waarin je geboren bent: ")
+geboortejaar_gebruiker = int(geboortejaar_gebruiker)
+
+#Toekomstige leeftijd uitrekenen
+Jaartal = 2030
+toekomstige_leeftijd = Jaartal - geboortejaar_gebruiker
+print(f"In 2030 zul je {toekomstige_leeftijd} jaar oud zijn.")
