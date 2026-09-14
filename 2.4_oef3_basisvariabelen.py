a9da99b1bdf67429013c5cefd6866e2935db1ced
LeonieCol <leoniecolemont@gmail.com>
2026-09-14 13:21:25 +0200

    oef 3

---
+++ 2.4_oef3_basisvariabelen.py
@@ -0,0 +1,12 @@
+#Je hebt €50 in je portemonnee
+a = 50
+
+#Een boek kost €12,50
+b = 12.50
+
+#Een tijdschrift kost €3,75
+c = 3.75
+
+#Berekening hoeveelheid geld na 2 boeken en 3 tijdschriften
+resultaat = a - 2 * b - 3 * c
+print(f"Na het kopen van 2 boeken en 3 tijdschriften heb je nog {resultaat} euro over.")
