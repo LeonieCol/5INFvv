53ef8d20471869d48e059e01bc40465bbfdea205
LeonieCol <leoniecolemont@gmail.com>
2026-09-14 14:03:08 +0200

    oef5&6

---
+++ 2.4_oef5_basisvariabelen.py
@@ -0,0 +1,13 @@
+#Twee variabelen maken en willekeurige gehele getallen aan toe voegen
+a = 1
+b = 2
+
+if a>b: 
+    print("a is groter dan b.")
+
+if a<b: 
+    print("a is kleiner dan b.")
+
+if a==b:
+    print("a is gelij aan b.")
+    
