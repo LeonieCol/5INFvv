b5680f7e1cc744a301c3edbba10e086f7b28029b
LeonieCol <leoniecolemont@gmail.com>
2026-09-21 14:05:24 +0200

    oef1-2

---
+++ 4.3_oef1_basisbewerkingen.py
@@ -0,0 +1,4 @@
+favoriete_kleuren = ["blauw", "groen", "rood"]
+favoriete_kleuren.append("geel")
+favoriete_kleuren.remove("blauw")
+print(favoriete_kleuren)
---
+++ 4.3_oef2_toegangtotlijstitems.py
@@ -0,0 +1,4 @@
+dagen = ["maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag"]
+print(dagen[0])
+print(dagen[-1])
+print(dagen[3])
