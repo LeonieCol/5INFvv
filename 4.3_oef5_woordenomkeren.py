e29df24bdfd8f636bda36f541912842ad9dbe31b
LeonieCol <leoniecolemont@gmail.com>
2026-09-25 13:24:42 +0200

    oef5

---
+++ 4.3_oef5_woordenomkeren.py
@@ -0,0 +1,8 @@
+woorden = ["Python", "is", "leuk"]
+
+omgekeerde_woorden = []
+for woord in woorden:
+    omgekeerd = woord[::-1]
+    omgekeerde_woorden.append(omgekeerd)
+    
+print(omgekeerde_woorden)
